ff0ae066c6aa40e77691bd2c01d476ef1fff6e7b
Roman Nenchuk <rmnnenchuk@gmail.com>
2025-03-10 21:36:39 +0200

    added default port in index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const __dirname = path.resolve();
 const app = express();
 
 const FRONTEND_URL_API = process.env.FRONTEND_URL_API;
-const BACKEND_PORT = process.env.PORT;
+const BACKEND_PORT = process.env.PORT || 4000;
 
 // Підключення до бази даних
 connectDB().catch(err => {
